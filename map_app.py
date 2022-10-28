6447d2f83dfcea991ed163dda1af37dc9816feaa
maty-z <yomatias_199@hotmail.com>
2022-10-28 02:09:44 -0300

    Add files via upload

---
+++ map_app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+data0 = pd.read_csv(r'https://raw.githubusercontent.com/maty-z/mapa_ramas/main/distribucion_establecimientos_productivos_descripciones_AMBA_salud_educacion_grupo0.csv?token=GHSAT0AAAAAAB2PWACUIW7GKF4YJ2OI6I2WY23MOYQ')
+data1 = pd.read_csv(r'https://raw.githubusercontent.com/maty-z/mapa_ramas/main/distribucion_establecimientos_productivos_descripciones_AMBA_salud_educacion_grupo1.csv?token=GHSAT0AAAAAAB2PWACV3OBPSLQHJDVOAY2YY23MP2A')
+data = pd.concat([data0,data1])[['ID','lat','lon','clae2_desc','empleo','empleo_rep']]
+
+'''data0_ = st.file_uploader('Data0')
+if data0_ is not None:
+    data0 = pd.read_csv(data0_)
+
+data1_ = st.file_uploader('Data1')
+if data1_ is not None:
+    data1 = pd.read_csv(data1_)
+
+
+data = pd.concat([data0,data1])[['ID','lat','lon','clae2_desc','empleo','empleo_rep']]
+'''
+fig = px.scatter_mapbox(data,lat = 'lat',lon = 'lon',  color='clae2_desc', size='empleo_rep', hover_data=['empleo'])
+fig.update_layout(mapbox_style = 'open-street-map')
+
+st.write('mapa')
+st.plotly_chart(fig)
