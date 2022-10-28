95c1f672b605652199833b0168fb89d639ce0cd8
maty-z <yomatias_199@hotmail.com>
2022-10-28 14:59:45 -0300

    Update map_app.py

--- map_app.py
+++ map_app.py
@@ -2,12 +2,33 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-data0 = pd.read_csv(r'https://raw.githubusercontent.com/maty-z/mapa_ramas/main/distribucion_establecimientos_productivos_descripciones_AMBA_salud_educacion_grupo0.csv')
-data1 = pd.read_csv(r'https://raw.githubusercontent.com/maty-z/mapa_ramas/main/distribucion_establecimientos_productivos_descripciones_AMBA_salud_educacion_grupo1.csv')
-data = pd.concat([data0,data1])[['ID','lat','lon','clae2_desc','empleo','empleo_rep']]
+data = pd.read_csv(r'https://raw.githubusercontent.com/maty-z/mapa_ramas/main/distribucion_establecimientos_productivos_descripciones_AMBA_filtro.csv')
 
-fig = px.scatter_mapbox(data,lat = 'lat',lon = 'lon',  color='clae2_desc', size='empleo_rep', hover_data=['empleo'])
+ramas_agrupadas = {}
+ramas_agrupadas['Transporte'] = ['Transporte terrestre y por tuberías',
+                                 'Transporte acuático',
+                                 'Transporte aéreo',]
+ramas_agrupadas['Salud y educacion'] = ['Enseñanza','Servicios de salud humana']
+ramas_agrupadas['Alimentacion'] = ['Elaboración de productos alimenticios']
+ramas_agrupadas['Energia'] = [ 'Suministro de electricidad, gas, vapor y aire acondicionado',
+                                'Captación, tratamiento y distribución de agua']    
+
+st.write('Mapa')
+
+filtro_sup = st.multiselect('Ramas',ramas_agrupadas,ramas_agrupadas)
+filtro = []
+for k in filtro_sup:
+    filtro = filtro+ramas_agrupadas[k]
+
+
+fig = px.scatter_mapbox(data[data.clae2_desc.isin(filtro)],lat = 'lat',lon = 'lon',  color='clae2_desc', 
+                size = 'empleo_rep', 
+                hover_data={'empleo':True,'empleo_rep':False,'lat':False,'lon':False,'clae2_desc':False},
+                #hover_data={'empleo':True,'lat':False,'lon':False,'clae2_desc':False},
+                hover_name='clae2_desc',
+                width = 1000,
+                labels={'clae2_desc':'Rama'}
+                )
 fig.update_layout(mapbox_style = 'open-street-map')
 
-st.write('mapa')
 st.plotly_chart(fig)
