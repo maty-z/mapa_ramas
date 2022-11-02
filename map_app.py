9a5c8a89a55c8ec160addce09788a4d76a14ca11
Maty <yomatias_199@hotmail.com>
2022-11-02 02:37:35 -0300

    add get query params

--- map_app.py
+++ map_app.py
@@ -30,8 +30,13 @@ ramas_agrupadas['Telecomunicaciones'] = ['Telecomunicaciones']
 
 
 st.title('Mapa: fuerza obrera en el AMBA')
+try: 
+    user_input = st.experimental_get_query_params()
+    mapa_inicial = user_input['Rama']
+except:
+    mapa_inicial = ['Transporte']
 
-filtro_sup = st.multiselect('Seleccione grupo de ramas',ramas_agrupadas,['Transporte'])
+filtro_sup = st.multiselect('Seleccione grupo de ramas',ramas_agrupadas,mapa_inicial)
 filtro = []
 for k in filtro_sup:
     filtro = filtro+ramas_agrupadas[k]
